69054af5abced971020372817fd0aff520fca66a
falcon1641 <94806141+falcon1641@users.noreply.github.com>
2022-03-14 13:43:44 -0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -87,126 +87,39 @@ end
 
 local function GenerateOwnerName()
     local names = {
-        [1] = {
-            name = "Jan Bloksteen",
-            citizenid = "DSH091G93"
-        },
-        [2] = {
-            name = "Jay Dendam",
-            citizenid = "AVH09M193"
-        },
-        [3] = {
-            name = "Ben Klaariskees",
-            citizenid = "DVH091T93"
-        },
-        [4] = {
-            name = "Karel Bakker",
-            citizenid = "GZP091G93"
-        },
-        [5] = {
-            name = "Klaas Adriaan",
-            citizenid = "DRH09Z193"
-        },
-        [6] = {
-            name = "Nico Wolters",
-            citizenid = "KGV091J93"
-        },
-        [7] = {
-            name = "Mark Hendrickx",
-            citizenid = "ODF09S193"
-        },
-        [8] = {
-            name = "Bert Johannes",
-            citizenid = "KSD0919H3"
-        },
-        [9] = {
-            name = "Karel de Grote",
-            citizenid = "NDX091D93"
-        },
-        [10] = {
-            name = "Jan Pieter",
-            citizenid = "ZAL0919X3"
-        },
-        [11] = {
-            name = "Huig Roelink",
-            citizenid = "ZAK09D193"
-        },
-        [12] = {
-            name = "Corneel Boerselman",
-            citizenid = "POL09F193"
-        },
-        [13] = {
-            name = "Hermen Klein Overmeen",
-            citizenid = "TEW0J9193"
-        },
-        [14] = {
-            name = "Bart Rielink",
-            citizenid = "YOO09H193"
-        },
-        [15] = {
-            name = "Antoon Henselijn",
-            citizenid = "QBC091H93"
-        },
-        [16] = {
-            name = "Aad Keizer",
-            citizenid = "YDN091H93"
-        },
-        [17] = {
-            name = "Thijn Kiel",
-            citizenid = "PJD09D193"
-        },
-        [18] = {
-            name = "Henkie Krikhaar",
-            citizenid = "RND091D93"
-        },
-        [19] = {
-            name = "Teun Blaauwkamp",
-            citizenid = "QWE091A93"
-        },
-        [20] = {
-            name = "Dries Stielstra",
-            citizenid = "KJH0919M3"
-        },
-        [21] = {
-            name = "Karlijn Hensbergen",
-            citizenid = "ZXC09D193"
-        },
-        [22] = {
-            name = "Aafke van Daalen",
-            citizenid = "XYZ0919C3"
-        },
-        [23] = {
-            name = "Door Leeferds",
-            citizenid = "ZYX0919F3"
-        },
-        [24] = {
-            name = "Nelleke Broedersen",
-            citizenid = "IOP091O93"
-        },
-        [25] = {
-            name = "Renske de Raaf",
-            citizenid = "PIO091R93"
-        },
-        [26] = {
-            name = "Krisje Moltman",
-            citizenid = "LEK091X93"
-        },
-        [27] = {
-            name = "Mirre Steevens",
-            citizenid = "ALG091Y93"
-        },
-        [28] = {
-            name = "Joosje Kalvenhaar",
-            citizenid = "YUR09E193"
-        },
-        [29] = {
-            name = "Mirte Ellenbroek",
-            citizenid = "SOM091W93"
-        },
-        [30] = {
-            name = "Marlieke Meilink",
-            citizenid = "KAS09193"
-        }
+ [1] = { name = "Bailey Sykes",          citizenid = "DSH091G93" },
+        [2] = { name = "Aroush Goodwin",        citizenid = "AVH09M193" },
+        [3] = { name = "Tom Warren",            citizenid = "DVH091T93" },
+        [4] = { name = "Abdallah Friedman",     citizenid = "GZP091G93" },
+        [5] = { name = "Lavinia Powell",        citizenid = "DRH09Z193" },
+        [6] = { name = "Andrew Delarosa",       citizenid = "KGV091J93" },
+        [7] = { name = "Skye Cardenas",         citizenid = "ODF09S193" },
+        [8] = { name = "Amelia-Mae Walter",     citizenid = "KSD0919H3" },
+        [9] = { name = "Elisha Cote",           citizenid = "NDX091D93" },
+        [10] = { name = "Janice Rhodes",        citizenid = "ZAL0919X3" },
+        [11] = { name = "Justin Harris",        citizenid = "ZAK09D193" },
+        [12] = { name = "Montel Graves",        citizenid = "POL09F193" },
+        [13] = { name = "Benjamin Zavala",      citizenid = "TEW0J9193" },
+        [14] = { name = "Mia Willis",           citizenid = "YOO09H193" },
+        [15] = { name = "Jacques Schmitt",      citizenid = "QBC091H93" },
+        [16] = { name = "Mert Simmonds",        citizenid = "YDN091H93" },
+        [17] = { name = "Rickie Browne",        citizenid = "PJD09D193" },
+        [18] = { name = "Deacon Stanley",       citizenid = "RND091D93" },
+        [19] = { name = "Daisy Fraser",         citizenid = "QWE091A93" },
+        [20] = { name = "Kitty Walters",        citizenid = "KJH0919M3" },
+        [21] = { name = "Jareth Fernandez",     citizenid = "ZXC09D193" },
+        [22] = { name = "Meredith Calhoun",     citizenid = "XYZ0919C3" },
+        [23] = { name = "Teagan Mckay",         citizenid = "ZYX0919F3" },
+        [24] = { name = "Kurt Bain",            citizenid = "IOP091O93" },
+        [25] = { name = "Burt Kain",            citizenid = "PIO091R93" },
+        [26] = { name = "Joanna Huff",          citizenid = "LEK091X93" },
+        [27] = { name = "Carrie-Ann Pineda",    citizenid = "ALG091Y93" },
+        [28] = { name = "Gracie-Mai Mcghee",    citizenid = "YUR09E193" },
+        [29] = { name = "Robyn Boone",          citizenid = "SOM091W93" },
+        [30] = { name = "Aliya William",        citizenid = "KAS009193" },
+        [31] = { name = "Rohit West",           citizenid = "SOK091093" },
+        [32] = { name = "Skylar Archer",        citizenid = "LOK091093" },
+        [33] = { name = "Jake Kumar",           citizenid = "AKA420609" },
     }
     return names[math.random(1, #names)]
 end
@@ -311,7 +224,7 @@ QBCore.Functions.CreateCallback('qb-phone:server:GetPhoneData', function(source,
             PhoneData.Hashtags = Hashtags
         end
 
-        local Tweets = MySQL.Sync.fetchAll('SELECT * FROM phone_tweets', {})
+        local Tweets = MySQL.Sync.fetchAll('SELECT * FROM phone_tweets WHERE `date` > NOW() - INTERVAL ? hour', {Config.TweetDuration})
         
         if Tweets ~= nil and next(Tweets) ~= nil then
             PhoneData.Tweets = Tweets
@@ -581,69 +494,6 @@ QBCore.Functions.CreateCallback('qb-phone:server:ScanPlate', function(source, cb
         cb(vehicleData)
     else
         TriggerClientEvent('QBCore:Notify', src, 'No Vehicle Nearby', 'error')
-        cb(nil)
-    end
-end)
-
-QBCore.Functions.CreateCallback('qb-phone:server:GetGarageVehicles', function(source, cb)
-    local Player = QBCore.Functions.GetPlayer(source)
-    local Vehicles = {}
-
-    local result = MySQL.Sync.fetchAll('SELECT * FROM player_vehicles WHERE citizenid = ?',
-        {Player.PlayerData.citizenid})
-    if result[1] ~= nil then
-        for k, v in pairs(result) do
-            local VehicleData = QBCore.Shared.Vehicles[v.vehicle]
-
-            local VehicleGarage = "None"
-            if v.garage ~= nil then
-                if Garages[v.garage] ~= nil then
-                    VehicleGarage = Garages[v.garage]["label"]
-                elseif GangGarages[v.garage] ~= nil then
-                    VehicleGarage = GangGarages[v.garage]["label"]
-                elseif JobGarages[v.garage] ~= nil then
-                    VehicleGarage = JobGarages[v.garage]["label"]
-                end
-            end
-
-            local VehicleState = "In"
-            if v.state == 0 then
-                VehicleState = "Out"
-            elseif v.state == 2 then
-                VehicleState = "Impounded"
-            end
-
-            local vehdata = {}
-
-            if VehicleData["brand"] ~= nil then
-                vehdata = {
-                    fullname = VehicleData["brand"] .. " " .. VehicleData["name"],
-                    brand = VehicleData["brand"],
-                    model = VehicleData["name"],
-                    plate = v.plate,
-                    garage = VehicleGarage,
-                    state = VehicleState,
-                    fuel = v.fuel,
-                    engine = v.engine,
-                    body = v.body
-                }
-            else
-                vehdata = {
-                    fullname = VehicleData["name"],
-                    brand = VehicleData["name"],
-                    model = VehicleData["name"],
-                    plate = v.plate,
-                    garage = VehicleGarage,
-                    state = VehicleState,
-                    fuel = v.fuel,
-                    engine = v.engine,
-                    body = v.body
-                }
-            end
-            Vehicles[#Vehicles+1] = vehdata
-        end
-        cb(Vehicles)
-    else
         cb(nil)
     end
 end)
@@ -962,29 +812,29 @@ end)
 RegisterNetEvent('qb-phone:server:UpdateTweets', function(NewTweets, TweetData)
     local src = source
     if Config.Linux then
-	local InsertTweet = MySQL.Async.insert('INSERT INTO phone_tweets (citizenid, firstName, lastName, message, date, url, picture, tweetid) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', {
-	TweetData.citizenid,
-	TweetData.firstName,
-	TweetData.lastName,
-	TweetData.message,
-	TweetData.date,
-	TweetData.url,
-	TweetData.picture,
-	TweetData.tweetId
-	})
-	TriggerClientEvent('qb-phone:client:UpdateTweets', -1, src, NewTweets, TweetData, false)
+	 local InsertTweet = MySQL.Async.insert('INSERT INTO phone_tweets (citizenid, firstName, lastName, message, date, url, picture, tweetid) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', {
+           TweetData.citizenid,
+           TweetData.firstName,
+           TweetData.lastName,
+           TweetData.message,
+           TweetData.date,
+           TweetData.url:gsub("[%<>\"()\' $]",""),
+           TweetData.picture,
+           TweetData.tweetId
+        })
+        TriggerClientEvent('qb-phone:client:UpdateTweets', -1, src, NewTweets, TweetData, false)
     else
-	local InsertTweet = MySQL.Async.insert('INSERT INTO phone_tweets (citizenid, firstName, lastName, message, date, url, picture, tweetid) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', {
-	TweetData.citizenid,
-	TweetData.firstName,
-	TweetData.lastName,
-	TweetData.message,
-	TweetData.time,
-	TweetData.url,
-	TweetData.picture,
-	TweetData.tweetId
-	})
-	TriggerClientEvent('qb-phone:client:UpdateTweets', -1, src, NewTweets, TweetData, false)		
+        local InsertTweet = MySQL.Async.insert('INSERT INTO phone_tweets (citizenid, firstName, lastName, message, date, url, picture, tweetid) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', {
+           TweetData.citizenid,
+           TweetData.firstName,
+           TweetData.lastName,
+           TweetData.message,
+           TweetData.time,
+           TweetData.url:gsub("[%<>\"()\' $]",""),
+           TweetData.picture,
+           TweetData.tweetId
+        })
+        TriggerClientEvent('qb-phone:client:UpdateTweets', -1, src, NewTweets, TweetData, false)		
     end
 end)
 
@@ -1184,6 +1034,17 @@ RegisterNetEvent('qb-phone:server:RemoveImageFromGallery', function(data)
     local Player = QBCore.Functions.GetPlayer(src)
     local image = data.image
     MySQL.Async.execute('DELETE FROM phone_gallery WHERE citizenid = ? AND image = ?',{Player.PlayerData.citizenid,image})
+end)
+
+RegisterNetEvent('qb-phone:server:sendPing', function(data)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    print(src, data)
+    if src ~= data then
+
+    else
+        TriggerClientEvent("QBCore:Notify", src, "You cannot ping yourself", "error")
+    end
 end)
 
 -- Command
